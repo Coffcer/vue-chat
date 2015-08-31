f738da4d8a9f62608bb8855538aee4729e63781d
Coffcer <coffce404@gmail.com>
2015-08-31 11:28:20 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require("path");
 var webpack = require("webpack");
 var vue = require("vue-loader");
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
+// var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
 	entry: {
