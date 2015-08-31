c7a83cf6f728304e97d0eedf3540c6cd65cf8043
Coffcer <coffce404@gmail.com>
2015-08-31 10:26:58 +0800

    update

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,16 +1,16 @@
 require('./card.less');
 
 module.exports = {
-	template: require('./card.tpl'),
-	props: ['store'],
-	data () {
-		return {
-			search: ''
-		};
-	},
-	methods: {
-		inputing () {
-			this.$dispatch('search', this.search);
-		}
-	}
+    template: require('./card.tpl'),
+    props: ['store'],
+    data () {
+        return {
+            search: ''
+        };
+    },
+    methods: {
+        inputing () {
+            this.$dispatch('search', this.search);
+        }
+    }
 };
